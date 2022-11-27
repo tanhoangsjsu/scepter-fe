e2f560fdabc4039d1891d40db2ee5355232d7dfd
Rishabh Gupta <rishabh.gupta01@sjsu.edu>
2022-11-26 16:43:26 -0800

    fix sending createRequest

--- src/Components/Search/Search.jsx
+++ src/Components/Search/Search.jsx
@@ -22,7 +22,7 @@ const Search = () => {
     const navigate = useNavigate();
     const user = useSelector((state)=> state.auth.login.currentUser);
 
-    const handleRequest = (e) =>{
+    const handleRequest = async (e) =>{
         e.preventDefault();
         const newRequest = {
             id:uuidv4(),
@@ -32,7 +32,7 @@ const Search = () => {
         }
         dispatch(createRequest(newRequest))
 
-	// Send Axios event to back-end to create new request
+	    // Send Axios event to back-end to create new request
         const res = await axios.post("v1/auth/createRequest", newRequest)
 
         handleBack()
